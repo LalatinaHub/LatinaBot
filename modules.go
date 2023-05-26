6d05f359f0c6b7fdfc38590c2dd2fcce76c8b801
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-05-26 11:02:31 +0700

    Add func

--- modules.go
+++ modules.go
@@ -1,6 +1,8 @@
 package latinabot
 
 import (
+	"os"
+
 	A "github.com/LalatinaHub/LatinaApi/common/account"
 	"github.com/LalatinaHub/LatinaBot/helper"
 	"github.com/NicoNex/echotron/v3"
@@ -27,4 +29,19 @@ func SendVPNToTopic(chatID int64, topicID int) {
 			DisableNotification: true,
 		})
 	}
+}
+
+func SendTextAsDocument(text, filename string, chatID int64) {
+	filename = filename + ".txt"
+	f, _ := os.Create(filename)
+	defer os.Remove(filename)
+	defer f.Close()
+
+	f.WriteString(text)
+
+	Client().SendDocument(echotron.NewInputFilePath(filename), chatID, nil)
+}
+
+func DocumentToAdmin(text, filename string) {
+	go SendTextAsDocument(text, filename, adminID)
 }
