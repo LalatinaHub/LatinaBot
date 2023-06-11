ea34b12318379f4c659b779a46dbd079f770cdf6
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2023-06-11 17:27:42 +0700

    Select relay

--- premium.go
+++ premium.go
@@ -12,7 +12,9 @@ import (
 )
 
 type PremiumVPNInfo struct {
-	VPN string
+	VPN    string
+	Domain string
+	CC     string
 }
 
 type PremiumDomainInfo struct {
@@ -69,30 +71,76 @@ func (b *bot) handlePremiumType(update *echotron.Update) stateFn {
 			},
 		})
 
-		return b.handlePremiumCreate
+		return b.handlePremiumServer
 	}
 
 	return b.handlePremiumType
 }
 
-func (b *bot) handlePremiumCreate(update *echotron.Update) stateFn {
+func (b *bot) handlePremiumServer(update *echotron.Update) stateFn {
 	if update.CallbackQuery != nil {
 		for _, domain := range domains {
 			if domain.Code == update.CallbackQuery.Data {
-				var message string
-				if member.CreatePremiumAccount(update.ChatID(), premiumVpnInfo.VPN, domain.Domain) {
-					message = "Akun berhasil dibuat !"
-				} else {
-					message = "Akun gagal dibuat !"
+				premiumVpnInfo.Domain = domain.Domain
+				premiumVpnInfo.CC = domain.Code
+
+				var (
+					relayCodes = []string{"Tanpa Relay"}
+				)
+				rows, err := db.New().Conn().Query("SELECT DISTINCT country_code FROM proxies WHERE vpn='shadowsocks'")
+				if err != nil {
+					fmt.Println(err)
 				}
 
-				b.DeleteMessage(update.ChatID(), update.CallbackQuery.Message.ID)
-				b.SendMessage(message, update.ChatID(), nil)
-				go b.menu(update)
+				for rows.Next() {
+					var relayCode sql.NullString
+					rows.Scan(&relayCode)
 
+					relayCodes = append(relayCodes, relayCode.String)
+				}
+
+				message := []string{"Silahkan pilih relay !"}
+				message = append(message, "\nSkema dengan relay:")
+				message = append(message, "<code>HP -> Server Fool -> Server Relay -> Internet</code>")
+				message = append(message, "\nSkema tanpa relay:")
+				message = append(message, "<code>HP -> Server Fool -> Internet</code>")
+
+				b.DeleteMessage(update.ChatID(), update.CallbackQuery.Message.ID)
+				go b.SendMessage(strings.Join(message[:], "\n"), update.ChatID(), &echotron.MessageOptions{
+					ParseMode: "HTML",
+					ReplyMarkup: echotron.InlineKeyboardMarkup{
+						InlineKeyboard: helper.BuildInlineKeyboardWithPage(relayCodes, 0),
+					},
+				})
 				break
 			}
 		}
+
+		return b.handlePremiumCreate
+	}
+
+	return b.handlePremiumServer
+}
+
+func (b *bot) handlePremiumCreate(update *echotron.Update) stateFn {
+	if update.CallbackQuery != nil {
+		var (
+			message   string
+			relayCode = update.CallbackQuery.Data
+		)
+
+		if len(relayCode) > 3 {
+			relayCode = premiumVpnInfo.CC
+		}
+		if member.CreatePremiumAccount(update.ChatID(), premiumVpnInfo.VPN, premiumVpnInfo.Domain, relayCode) {
+			message = "Akun berhasil dibuat !"
+		} else {
+			message = "Akun gagal dibuat !"
+		}
+
+		b.DeleteMessage(update.ChatID(), update.CallbackQuery.Message.ID)
+		b.SendMessage(message, update.ChatID(), nil)
+		go b.menu(update)
 
 		return b.handleMessage
 	}
